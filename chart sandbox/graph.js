9e58db05ce247a8b81a495c244dbae6198db5027
AaronMullan <aaroncmullan@gmail.com>
2020-01-15 11:31:08 -0800

    chart sandboxing

---
+++ chart sandbox/graph.js
@@ -0,0 +1,19 @@
+// create data
+var data = [
+  ['African American', 8269],
+  ['Asian', 580],
+  ['Latino', 2006],
+  ['White', 638],
+];
+
+// create a chart
+const chart = anychart.bar();
+
+// create a bar series and set the data
+var series = chart.bar(data);
+
+// set the container id
+chart.container('container');
+
+// initiate drawing the chart
+chart.draw();
