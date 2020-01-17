ed80bc57a8d42b737ac6917cfa2f592d309ce23b
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-17 11:15:07 -0800

    removed unused files anc console.logs, added README.md

--- __tests__/detentions.test.js
+++ __tests__/detentions.test.js
@@ -105,7 +105,6 @@ describe('app routes', () => {
     return request(app)
       .get(`/api/v1/detentions/${detention._id}`)
       .then(res => {
-        console.log(res.body);
         expect(res.body).toEqual({
           _id: expect.any(String),
           bookingNumber: '12345678',
